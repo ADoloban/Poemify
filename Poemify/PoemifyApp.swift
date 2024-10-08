33da573d20e810904d881062ce381f146f118f8a
Artem Doloban <artemdoloban@gmail.com>
2024-10-08 22:42:08 +0300

    Initial commit

---
+++ Poemify/PoemifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemifyApp.swift
+//  Poemify
+//
+//  Created by Artem Doloban on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PoemifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
