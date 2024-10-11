32b1fe27c0a7fd3df729a3a7e79b8e0e72fe08ff
Artem Doloban <artemdoloban@gmail.com>
2024-10-11 10:44:00 +0300

    create PoemRequestAPIService

---
+++ Poemify/Services/PoemRequestType.swift
@@ -0,0 +1,13 @@
+//
+//  PoemRequestType.swift
+//  Poemify
+//
+//  Created by Artem Doloban on 11.10.2024.
+//
+
+import Foundation
+
+enum PoemRequestType {
+    case allPoems
+    case search(author: String?, title: String?, numberOfLines: Int?, resultCount: Int?, random: Bool)
+}
