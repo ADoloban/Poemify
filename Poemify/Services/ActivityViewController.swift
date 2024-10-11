75990e4cd2cb138da4ba135459d1cddd17f5867a
Artem Doloban <artemdoloban@gmail.com>
2024-10-11 10:48:45 +0300

    create ActivityViewController

---
+++ Poemify/Services/ActivityViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ActivityViewController.swift
+//  Poemify
+//
+//  Created by Artem Doloban on 10.10.2024.
+//
+
+import SwiftUI
+import UIKit
+
+// UIViewControllerRepresentable для ActivityViewController
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
