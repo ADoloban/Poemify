269ff222718ae9b88a89f9529cb0efb575454433
Artem Doloban <artemdoloban@gmail.com>
2024-10-21 11:03:38 +0300

    change SavedPoemsView

--- Poemify/View/SavedPoemsView.swift
+++ Poemify/View/SavedPoemsView.swift
@@ -8,22 +8,25 @@
 import SwiftUI
 
 struct SavedPoemsView: View {
-    @State private var savedPoems: [Poem] = []  
+    @EnvironmentObject var savedPoemsViewModel: SavedPoemsViewModel // Підключаємо ViewModel
     
     var body: some View {
-        NavigationView {
-            VStack {
-                if savedPoems.isEmpty {
-                    Text("You haven't saved any poems yet!")
-                        .font(.title3)
-                        .foregroundColor(.gray)
-                } else {
-                    List(savedPoems, id: \.self) { poem in
-                        Text(poem.title)
-                    }
-                }
+        if savedPoemsViewModel.savedPoems.isEmpty {
+            Text("No saved poems yet.")
+                .foregroundColor(.gray)
+        } else {
+            List(savedPoemsViewModel.savedPoems, id: \.self) { poem in
+                PoemCell(poem: poem) // Використовуємо PoemCell як у ContentView
+                    .padding(.vertical, 10)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    .background(
+                        NavigationLink("",
+                                       destination: PoemDetailView(poem: poem)
+                            .environmentObject(savedPoemsViewModel)
+                                      ))
             }
-            .navigationTitle("Saved Poems")
+            .listStyle(PlainListStyle())
+            .navigationTitle("Saved Poems")// Використовуємо PlainListStyle як у ContentView
         }
     }
 }
