290510908e8a16d0bdd5deec013bb5ec4a34afb3
Artem Doloban <artemdoloban@gmail.com>
2024-11-17 22:24:55 +0200

    create CollectionCell and CollectionViewModel

---
+++ Poemify/View/Collection/CollectionCell.swift
@@ -0,0 +1,53 @@
+//
+//  CollectionCell.swift
+//  Poemify
+//
+//  Created by Artem Doloban on 17.11.2024.
+//
+
+import SwiftUI
+
+struct CollectionCell: View {
+    var collection: PoemCollection
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Text(collection.name)
+                    .font(.system(size: 20, weight: .bold, design: .rounded))
+                    .foregroundColor(Color("01204E"))
+                
+                Spacer()
+                if !collection.poems.isEmpty {
+                    Text("\(collection.poems.count) Poems")
+                        .font(.callout)
+                        .foregroundColor(Color("5F6F65"))
+                }
+            }
+            Divider()
+                .background(Color("5F6F65"))
+            
+            if !collection.poems.isEmpty {
+                VStack(alignment: .leading, spacing: 4) {
+                    ForEach(collection.poems.prefix(3), id: \.title) { poem in
+                        Text(poem.title)
+                            .font(.system(size: 17))
+                            .foregroundColor(.black)
+                            .lineLimit(1)
+                    }
+                }
+            } else {
+                Text("No poems available")
+                    .font(.system(size: 17))
+                    .italic()
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
+        .background(Color("C6EBC5"))
+        .cornerRadius(10)
+        .shadow(radius: 2)
+        .frame(maxWidth: .infinity)
+    }
+}
+
---
+++ Poemify/View/Collection/CreateCollectionModalView.swift
@@ -0,0 +1,68 @@
+//
+//  CreateCollectionModalView.swift
+//  Poemify
+//
+//  Created by Artem Doloban on 17.11.2024.
+//
+
+import SwiftUI
+
+struct CreateCollectionModalView: View {
+    @Binding var isPresented: Bool
+    @Binding var collectionName: String
+    var onSave: () -> Void
+
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    isPresented = false
+                    clearFields()
+                }
+
+            VStack(spacing: 20) {
+                Text("Create New Collection")
+                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                    .foregroundStyle(Color("FD8B51"))
+
+                VStack(spacing: 20) {
+                    StyledTextField(
+                        placeholder: "Collection Name",
+                        text: $collectionName,
+                        isSecure: false
+                    )
+                    .foregroundColor(Color("EBE3D5"))
+                }
+                .padding(.horizontal)
+
+                Button(action: {
+                    isPresented = false
+                    onSave()
+                    clearFields()
+                }) {
+                    Text("Create")
+                }
+                .padding(10)
+                .font(.system(size: 18, weight: .bold, design: .rounded))
+                .background(collectionName.isEmpty ? Color.gray : Color("FD8B51"))
+                .foregroundColor(collectionName.isEmpty ? Color.white.opacity(0.7) : Color("C6EBC5"))
+                .cornerRadius(20)
+                .disabled(collectionName.isEmpty)
+                .padding(.top)
+            }
+            .frame(width: 300, height: 200)
+            .background(Color(.white).opacity(0.4))
+            .cornerRadius(20)
+            .shadow(radius: 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+        }
+    }
+
+    private func clearFields() {
+        collectionName = ""
+    }
+}
